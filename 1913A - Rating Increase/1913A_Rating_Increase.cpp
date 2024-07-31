790ef9b87284e958d99c069725aa3c97da4d87b3
Asfia-oishy <asfiamoon126@gmail.com>
2024-07-31 23:18:46 +0600

    accepted

--- 1913A - Rating Increase/1913A_Rating_Increase.cpp
+++ 1913A - Rating Increase/1913A_Rating_Increase.cpp
@@ -1,28 +1,77 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    for (int x = 1; x <= t; x++)
     {
         string ab;
-        cin>>ab;
-        int len = ab.size();
-        for(int i=0;i<len;i++)
+        vector<char> a, b;
+        a.clear();
+        b.clear();
+        cin >> ab;
+        bool flag = true;
+        int len = ab.size(), br = -1;
+        for (int i = 0; i < len - 1; i++)
         {
-            if(ab[i]>=48 && ab[i]<=57)
+
+            if (ab[i + 1] != '0')
             {
-                
-            }
-            else
-            {
-                cout<<-1<<endl;
+                br = i;
+                break;
             }
         }
+        if (br != -1)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                if (j <= br)
+                {
+                    a.push_back(ab[j]);
+                }
+
+                else
+                    b.push_back(ab[j]);
+            }
+        }
+
+        if (a.size() == b.size())
+        {
+            int c = 0;
+            for (int j = 0; j < a.size(); j++)
+            {
+                if (a[j] < b[j])
+                {
+                    break;
+                }
+                else if (a[j] == b[j])
+                    c++;
+                else
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (c == a.size())
+                flag = false;
+        }
+        else if (a.size() > b.size())
+            flag = false;
+
+        if (flag && br != -1)
+        {
+            for (int j = 0; j < a.size(); j++)
+                cout << a[j];
+            cout << " ";
+            for (int j = 0; j < b.size(); j++)
+                cout << b[j];
+            cout << endl;
+        }
+        else
+            cout << -1 << endl;
     }
-    
 
     return 0;
 }
