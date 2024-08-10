b26959f006efc99b0d21ac1a80622a7eed55127f
Asfia-oishy <asfiamoon126@gmail.com>
2024-08-10 11:28:57 +0600

    accepted

--- 146A - Lucky Ticket/Lucky_Ticket.cpp
+++ 146A - Lucky Ticket/Lucky_Ticket.cpp
@@ -4,5 +4,36 @@ typedef long long int ll;
 
 int main()
 {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    bool flag = true;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] != '4' && s[i] != '7')
+        {
+            cout << "NO" << endl;
+            flag = false;
+            break;
+        }
+    }
+    if (flag)
+    {
+        int x = 0, y = 0;
+        for (int i = 0; i < n / 2; i++)
+        {
+            x += (s[i] - '0');
+        }
+        for (int j = n / 2; j < n; j++)
+        {
+            y += (s[j] - '0');
+        }
+        if (x == y)
+            cout<<"YES" << endl;
+        else
+            cout<<"NO" << endl;
+    }
     return 0;
 }
