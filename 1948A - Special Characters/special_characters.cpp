e03e1eba5a6415d3a55f21880964e5e3453cb7fc
Asfia-oishy <asfiamoon126@gmail.com>
2024-08-13 15:37:42 +0600

    accepted

--- 1948A - Special Characters/special_characters.cpp
+++ 1948A - Special Characters/special_characters.cpp
@@ -1,7 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n % 2 == 1)
+        {
+            cout << "NO" << endl;
+            continue;
+        }
+        cout<<"YES"<<endl;
+        char ch='A';
+
+        for(int i=0;i<n;i++)
+        {
+            if(i!=0 && i%2==0)
+            {
+                ch+=1;
+            }
+            cout<<ch;
+            
+        }
+        cout<<endl;
+    }
     return 0;
 }
