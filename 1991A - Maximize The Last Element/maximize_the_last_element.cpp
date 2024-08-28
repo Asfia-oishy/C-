98ab1693c4a69e50f3d91365657a3688d7ce5028
Asfia-oishy <asfiamoon126@gmail.com>
2024-08-28 12:47:29 +0600

    accepted

--- 1991A - Maximize The Last Element/maximize_the_last_element.cpp
+++ 1991A - Maximize The Last Element/maximize_the_last_element.cpp
@@ -1,50 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool comp(pair<int, int> &a, pair<int, int> &b)
-{
-    return a.first >= b.first;
-}
+
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        vector<pair<int, int>> v;
-        v.clear();
 
         int n;
         cin >> n;
-        int arr[n], max_ = 0, ind;
+        int arr[n], max_ = 0;
         for (int i = 0; i < n; i++)
         {
             cin >> arr[i];
-            v.push_back(make_pair(arr[i], i + 1));
-
-            if (arr[i] > max_)
+            if ((i + 1) % 2 == 1)
             {
-                max_ = arr[i];
-                ind = i + 1;
+                max_ = max(max_, arr[i]);
             }
         }
-        sort(v.begin(), v.end(), comp);
 
-        if (ind % 2 == 1)
-        {
-            cout << max_ << endl;
-        }
-        else
-        {
-            for (int i = 0; i < v.size(); i++)
-            {
-                int index = v[i].second;
-                if (index % 2 == 1)
-                {
-                    cout << v[i].first << endl;
-                    break;
-                }
-            }
-        }
+        cout << max_ << endl;
     }
     return 0;
 }
