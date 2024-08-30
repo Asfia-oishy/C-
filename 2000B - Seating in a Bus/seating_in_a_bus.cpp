840e31d2f82d5be79d60fdfcaa5427e6fd7bc4c6
Asfia-oishy <asfiamoon126@gmail.com>
2024-08-30 23:49:40 +0600

    accepted

---
+++ 2000B - Seating in a Bus/seating_in_a_bus.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        bool flag = true;
+        cin >> n;
+        int arr[n + 1], occupied[n + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            occupied[i] = 0;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> arr[i];
+            occupied[arr[i]] = 1;
+            if (i > 1)
+            {
+
+                if (arr[i] == 1 && occupied[2] == 0)
+                {
+                    cout << "NO" << endl;
+                    flag = false;
+                }
+                else if (arr[i] == n && occupied[n - 1] == 0)
+                {
+                    cout << "NO" << endl;
+                    flag = false;
+                }
+                else if (occupied[arr[i] - 1] == 0 && occupied[arr[i] + 1] == 0)
+                {
+                    cout << "NO" << endl;
+                    flag = false;
+                }
+            }
+        }
+
+        if (flag)
+        {
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
