f6792ff805404176a3d9b27eab57ae74e1d80dda
Asfia-oishy <asfiamoon126@gmail.com>
2024-08-31 02:04:12 +0600

    accepted

--- 2000B - Seating in a Bus/seating_in_a_bus.cpp
+++ 2000B - Seating in a Bus/seating_in_a_bus.cpp
@@ -15,28 +15,21 @@ int main()
         {
             occupied[i] = 0;
         }
+        for (int i = 1; i <= n; i++)
+
+            cin >> arr[i];
 
         for (int i = 1; i <= n; i++)
         {
-            cin >> arr[i];
             occupied[arr[i]] = 1;
             if (i > 1)
             {
 
-                if (arr[i] == 1 && occupied[2] == 0)
+                if ((arr[i] == 1 && occupied[2] == 0) || (arr[i] == n && occupied[n - 1] == 0) || (occupied[arr[i] - 1] == 0 && occupied[arr[i] + 1] == 0))
                 {
                     cout << "NO" << endl;
                     flag = false;
-                }
-                else if (arr[i] == n && occupied[n - 1] == 0)
-                {
-                    cout << "NO" << endl;
-                    flag = false;
-                }
-                else if (occupied[arr[i] - 1] == 0 && occupied[arr[i] + 1] == 0)
-                {
-                    cout << "NO" << endl;
-                    flag = false;
+                    break;
                 }
             }
         }
