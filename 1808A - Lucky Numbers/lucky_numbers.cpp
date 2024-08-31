cf68c0350199a741566f7c7fb12a588790b3ccc3
Asfia-oishy <asfiamoon126@gmail.com>
2024-08-31 12:41:16 +0600

    accepted

--- 1808A - Lucky Numbers/lucky_numbers.cpp
+++ 1808A - Lucky Numbers/lucky_numbers.cpp
@@ -7,6 +7,24 @@ int main()
     cin >> t;
     while (t--)
     {
+        int l, r;
+        cin >> l >> r;
+        int max_ = 0, ans;
+        for (int i = l; i <= r; i++)
+        {
+            string s = to_string(i);
+            sort(s.begin(), s.end());
+            int diff = s[s.size() - 1] - s[0];
+            if (diff >= max_)
+            {
+                max_ = diff;
+                ans = i;
+                if (max_ == 9)
+                    break;
+            }
+        }
+
+        cout << ans << endl;
     }
     return 0;
 }
