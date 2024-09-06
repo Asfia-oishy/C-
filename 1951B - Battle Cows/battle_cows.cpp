2606cc98f8b5b44eea1e8e6fb5f4bdb9c95e6a89
Asfia-oishy <asfiamoon126@gmail.com>
2024-09-06 21:04:10 +0600

    accepted

--- 1951B - Battle Cows/battle_cows.cpp
+++ 1951B - Battle Cows/battle_cows.cpp
@@ -1,110 +1,58 @@
+
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-bool isPossible(int mid, int k, int arr[], int n)
+void solve()
 {
-    int win = 0, index = k;
-    bool flag = false;
-    for (int i = 1; i < k; i++)
+    ll n, k;
+    cin >> n >> k;
+    k--;
+    vector<ll> v(n);
+    vector<ll> a(n);
+    ll ans2 = 0;
+
+    for (ll i = 0; i < n; i++)
     {
-        if (arr[i] > arr[k])
+        cin >> v[i];
+        a[i] = v[i];
+    }
+    swap(a[0], a[k]);
+    for (ll i = 1; i < n; i++)
+    {
+        if (a[i] < a[0])
+            ans2++;
+        else
+            break;
+    }
+    for (ll i = 0; i < k; i++)
+    {
+        if (v[i] > v[k])
         {
-            if (k - i > i - 1 - 1 || i == 1)
-            {
-                int tmp = arr[k];
-                arr[k] = arr[i];
-                arr[i] = tmp;
-                index = i;
-                break;
-            }
-            else
-            {
-                flag = true;
-            }
-        }
-        if (i == k - 1 || flag)
-        {
-            int tmp = arr[k];
-            arr[k] = arr[1];
-            arr[1] = tmp;
-            index = 1;
+            swap(v[i], v[k]);
+            k = i;
             break;
         }
     }
-    
-
-    for (int i = 1; i <= n; i++)
+    ll ans = 0;
+    if (k != 0)
+        ans++;
+    for (ll i = k + 1; i < n; i++)
     {
-        if (index == 1)
-        {
-            if (i > index)
-            {
-                if (arr[i] < arr[index])
-                    win += 1;
-                else
-                    break;
-            }
-        }
+        if (v[i] < v[k])
+            ans++;
         else
-        {
-            if (i == index)
-            {
-                win++;
-            }
-            if (i > index)
-            {
-                if (arr[i] < arr[index])
-                    win += 1;
-            }
-        }
+            break;
     }
-    // cout << win << " " << k << " " << mid << endl;
-    if (win >= mid)
-        return true;
-    return false;
+    cout << max(ans2, ans) << endl;
 }
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n, k;
-        cin >> n >> k;
-        int arr[n + 1], ans = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            cin >> arr[i];
-        }
-        if (n == 2)
-        {
-            if (max(arr[1], arr[2]) == arr[k])
-                cout << 1 << endl;
-            else
-                cout << 0 << endl;
-            continue;
-        }
-        else
-        {
-            int l = 0, h = n - 1;
-            while (l <= h)
-            {
-                int mid = l + (h - l) / 2;
 
-                if (isPossible(mid, k, arr, n))
-                {
-                    ans = max(ans, mid);
-                    l = mid + 1;
-                }
-                else
-                {
-                    h = mid - 1;
-                }
-            }
-
-            cout << ans << endl;
-        }
-    }
+    ll tt;
+    cin >> tt;
+    while (tt--)
+        solve();
     return 0;
 }
